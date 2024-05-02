c54d22a27b13f8d59f65be1602496516ec3bdfa2
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-02 17:51:39 +0200

    first commit

---
+++ mail/mail.js
@@ -0,0 +1,8 @@
+'use strict'
+
+ const mailUtente = prompt('inserire email');
+console.log(mailUtente);
+
+const mailValide = ['esempio1@gmail.com', 'esempio2@gmail.com', 'esempio3@outlook.it'];
+
+console.log(mailValide[1]);
