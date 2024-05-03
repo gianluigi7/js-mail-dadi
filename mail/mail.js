d27a6ca12cf643bbf4bb5d107cc0b9721839e526
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-05-03 11:15:57 +0200

    correzione

--- mail/mail.js
+++ mail/mail.js
@@ -2,23 +2,42 @@
 
 
 let mailValide = ['esempio1@gmail.com', 'esempio2@gmail.com', 'esempio3@outlook.it'];
+let canAccess = false;
 
-const mailUtente = prompt('inserire email');
+const mailUtente = prompt('inserire email').toLowerCase();
 
-    if (mailUtente === mailValide[0]) {
-    console.log(mailUtente);
+//    if (mailUtente === mailValide[0]) {
+//    console.log(mailUtente);
 
-} 
-else if (mailUtente === mailValide[1]) {
-    console.log(mailUtente);
+//} 
+//else if (mailUtente === mailValide[1]) {
+//    console.log(mailUtente);
 
-} 
-else if (mailUtente === mailValide[2]) {
-    console.log(mailUtente);
+//} 
+//else if (mailUtente === mailValide[2]) {
+ //   console.log(mailUtente);
 
-} 
-  else {
-    alert('email non valida');
+//} 
+//  else {
+//    alert('email non valida');
     
+//}
+
+for(let i=0; i < mailValide.length; i++) {
+    if (mailValide[i] === mailUtente) {
+        canAccess = true;
+        console.log(mailUtente)
+        break;
+    }
+}
+
+
+
+if (canAccess === true) {
+    console.log('puoi accedere');
+}
+
+else {
+    console.log('Non puoi accedere');
 }
 
